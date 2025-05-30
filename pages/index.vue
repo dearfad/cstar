404f781a155d1b5c73a4a7d48e3daf5f23e72086
dearfad <dearfad@gmail.com>
2025-05-30 17:45:26 +0800

    add home

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-sheet class="d-flex flex-column w-100 w-md-50 mx-auto justify-center align-center pt-4 ga-4">
+    <div class="text-h3 font-weight-bold text-blue my-10">CSTAR</div>
+    <v-text-field v-model="knowledge" label="Knowledge" outlined clearable class="w-100" />
+    <v-btn text="SEARCH" size="large" block />
+  </v-sheet>
+</template>
+
+<script setup lang="ts">
+const knowledge = ref('')
+</script>
